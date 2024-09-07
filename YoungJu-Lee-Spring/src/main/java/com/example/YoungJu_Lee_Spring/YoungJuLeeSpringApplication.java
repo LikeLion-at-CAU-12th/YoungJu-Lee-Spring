09ea0cca1704aa230fde46b0d23884f72ee9e8a4
JooJooda <liyingzhou375@gmail.com>
2024-09-07 16:08:36 +0900

    chore : initial settings

---
+++ YoungJu-Lee-Spring/src/main/java/com/example/YoungJu_Lee_Spring/YoungJuLeeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.YoungJu_Lee_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungJuLeeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungJuLeeSpringApplication.class, args);
+	}
+
+}
---
+++ YoungJu-Lee-Spring/src/test/java/com/example/YoungJu_Lee_Spring/YoungJuLeeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YoungJu_Lee_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungJuLeeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
