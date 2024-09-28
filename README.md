cb9e2ac808689eb909305241eb4b552a46fa630a
JooJooda <liyingzhou375@gmail.com>
2024-09-28 22:39:09 +0900

    feat : add service

--- README.md
+++ README.md
@@ -9,3 +9,27 @@
 </div>
 </details>
 
+<details>
+<summary>week3.Spring-data-JPA-session</summary>
+<div markdown="1">
+
+### Paging Test
+
+![스크린샷 2024-09-28 11722](https://github.com/user-attachments/assets/93ad4c86-721e-472c-acad-dc3acb1dd828)
+
+
+![스크린샷 2024-09-28 19112](https://github.com/user-attachments/assets/8c8c5d9b-aeae-4c26-87c3-38f46922a10a)
+
+### findByAgeGreaterThanEqualOrderByUsernameAsc
+
+
+![스크린샷 2024-09-28 22340](https://github.com/user-attachments/assets/12f2914c-0138-4423-a42a-5324c4562311)
+
+
+### findByUsernameStartsWith 
+
+![스크린샷 2024-09-28 22172](https://github.com/user-attachments/assets/71065de2-8a57-492e-99c8-8649318f54ed)
+
+
+</div>
+</details>
---
+++ YoungJu-Lee-Spring/src/main/java/com/example/YoungJu_Lee_Spring/Service/MemberService.java
@@ -0,0 +1,48 @@
+package com.example.YoungJu_Lee_Spring.Service;
+
+import com.example.YoungJu_Lee_Spring.domain.Member;
+import com.example.YoungJu_Lee_Spring.repository.MemberJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberJpaRepository memberJpaRepository;
+
+    public Page<Member> getMembersByPage(int page, int size){
+        Pageable pageable = PageRequest.of(page, size, Sort.by("username").ascending());
+        return memberJpaRepository.findAll(pageable);
+
+    }
+
+    public Page<Member> getMembersByAgeGreaterthan20(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        return memberJpaRepository.findByAgeGreaterThanEqualOrderByUsernameAsc(20, pageable);
+    }
+
+    public Page<Member> getMembersByUsernameStartsWith(int page, int size, String username){
+        Pageable pageable = PageRequest.of(page, size);
+        return memberJpaRepository.findByUsernameStartsWith(username, pageable);
+    }
+
+    public void printMembersByPage(int page, int size) {
+        Page<Member> memberPage = getMembersByPage(page, size);
+        List<Member> members = memberPage.getContent();
+
+        for (Member member : members) {
+            System.out.println("ID: " + member.getId() + ", Username: " + member.getUsername());
+        }
+    }
+
+    public void printMember(Member member) {
+        System.out.println("ID: " + member.getId() + ", Username: " + member.getUsername() + ", Age: " + member.getAge());
+    }
+
+}
